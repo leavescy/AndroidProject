dd0d35ac7b10eb11cde335953ff1b6ce3c1497ff
leavescy <njuchenye@gmail.com>
2018-06-25 21:05:15 +0800

    deal with handler

--- handler_study/app/src/main/java/com/example/chenye/handler_study/MainActivity.java
+++ handler_study/app/src/main/java/com/example/chenye/handler_study/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.chenye.handler_study;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void beginBtn(View view) {
+
+    }
+    Handler handler = new Handler(){
+        public void handleMessage(Message msg){
+
+        }
+    };
 }
