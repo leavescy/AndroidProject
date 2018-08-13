e1eb0f02d857d6f4316ee653c2a7711a08af9ee0
leavescy <njuchenye@gmail.com>
2018-08-13 17:52:30 +0800

    基础menu

--- menuTest/app/src/main/java/com/example/chenye/menutest/MainActivity.java
+++ menuTest/app/src/main/java/com/example/chenye/menutest/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.chenye.menutest;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        ImageButton iv_button = findViewById(R.id.iv_back);
+        ImageButton pull_down_button = findViewById(R.id.pull_down);
+        iv_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        pull_down_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.start:
+                Toast.makeText(this, "You clicked start", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.over:
+                Toast.makeText(this, "You clicked over", Toast.LENGTH_LONG).show();
+                break;
+            default:
+
+        }
+        return true;
+    }
+
 }
