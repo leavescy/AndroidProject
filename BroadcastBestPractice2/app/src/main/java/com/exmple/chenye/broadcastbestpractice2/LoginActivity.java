de83b0afa4852d2bc0e54f328ad78c2aa7f31637
chenye3 <chenye3@corp.netease.com>
2018-09-20 19:29:57 +0800

    广播2

---
+++ BroadcastBestPractice2/app/src/main/java/com/exmple/chenye/broadcastbestpractice2/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.exmple.chenye.broadcastbestpractice2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends BaseActivity{
+    private EditText accountEdit;
+    private EditText passwordEdit;
+    private Button loginBtn;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        accountEdit = findViewById(R.id.account);
+        passwordEdit = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.login);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String account = accountEdit.getText().toString();
+                String password = passwordEdit.getText().toString();
+                if("admin".equals(account) && "123456".equals(password)){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Toast.makeText(LoginActivity.this, "account or passowrd is invalid", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
